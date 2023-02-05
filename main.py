4c4d9cf1524de294ab175161e2825b155309138d
priyam-priyanshu <76548882+priyam-priyanshu@users.noreply.github.com>
2023-02-06 00:30:24 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  5 10:24:04 2023
+
+@author: Priyanshu Priyam
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+df = pd.read_csv("Amazon Foods\Reviews.csv")
+df.head()
